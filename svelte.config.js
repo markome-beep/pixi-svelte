83400f3580912f0a8059ed7711377861395220ff
markome-beep <guyofsoccer@gmail.com>
2025-02-08 20:04:04 -0600

    new config rewrite

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.BASE_PATH || "", // Needed for GitHub Pages
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
-	},
-
+	}
 };
 
 export default config;
